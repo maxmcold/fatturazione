71e9ff34386c03c9a8c1e42a95717caaecad7090
mdelvecchio <massimo.delvecchio@upstreamsystems.com>
2017-01-31 19:16:08 +0200

    .

--- fatturazione/components/com_fatturazione/controller.php
+++ fatturazione/components/com_fatturazione/controller.php
@@ -3,21 +3,12 @@
  */
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
+
 /**
  * Fatturazione Component Controller
  *
- * @since  0.0.1
  */
 class FatturazioneController extends JControllerLegacy
 {
-	public function display($cachable = false, $urlparams = array())
-	{
-		return $this;
-	}
-	
-	public function list($cachable = false, $urlparams = array())
-	{
-		return $this;
-	}
-	
 }
--- fatturazione/components/com_fatturazione/fatturazione.php
+++ fatturazione/components/com_fatturazione/fatturazione.php
@@ -2,13 +2,15 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
-// Get an instance of the controller prefixed by HelloWorld
+
+
+$test = 'mytest';
+// Get an instance of the controller prefixed by Fatturazione
 $controller = JControllerLegacy::getInstance('Fatturazione');
- 
+
 // Perform the Request task
 $input = JFactory::getApplication()->input;
 $controller->execute($input->getCmd('task'));
- 
+
 // Redirect if set by the controller
 $controller->redirect();
