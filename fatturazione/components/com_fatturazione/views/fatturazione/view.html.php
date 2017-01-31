42e001dfaa74e1ee11a024be0d465bac41e7a4e7
mdelvecchio <massimo.delvecchio@upstreamsystems.com>
2017-01-31 13:25:18 +0200

    manual commit

---
+++ fatturazione/components/com_fatturazione/views/fatturazione/view.html.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_fatturazione
+ *
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ *
+ * @since  0.0.1
+ */
+class FatturazioneViewFatturazione extends JViewLegacy
+{
+	/**
+	 * Display the Fatturazione view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Assign data to the view
+		$this->msg = 'Hello Strunz';
+ 
+		// Display the view
+		parent::display($tpl);
+	}
+}
